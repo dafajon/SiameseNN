302768aaf32cd082d0ee31505d8bd111486c1e87
dafajon <dorukhan.afacan@globalmaksimum.com>
2019-08-27 10:36:50 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A PyTorch implementation of Siamese Neural Network implemented on recycables dataset.',
+    author='Dorukhan Afacan',
+    license='MIT',
+)
